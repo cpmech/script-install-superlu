cfc6d26e0679f24d2475a494c5a380aa1ef63ef7
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-28 08:34:26 +1000

    Impl script to compile super lu

--- README.md
+++ README.md
@@ -14,6 +14,21 @@ bash install-superlu.bash
 Output of `tree /usr/local/include/superlu`
 
 ```text
+/usr/local/include/superlu
+├── colamd.h
+├── slu_cdefs.h
+├── slu_Cnames.h
+├── slu_dcomplex.h
+├── slu_ddefs.h
+├── slu_scomplex.h
+├── slu_sdefs.h
+├── slu_util.h
+├── slu_zdefs.h
+├── superlu_config.h
+├── superlu_enum_consts.h
+└── supermatrix.h
+
+1 directory, 12 files
 ```
 
 ## Installed lib files
@@ -21,4 +36,10 @@ Output of `tree /usr/local/include/superlu`
 Output of `tree /usr/local/lib/superlu`
 
 ```text
+/usr/local/lib/superlu
+├── libsuperlu.so -> libsuperlu.so.6
+├── libsuperlu.so.6 -> libsuperlu.so.6.0.1
+└── libsuperlu.so.6.0.1
+
+1 directory, 3 files
 ```
--- install-superlu.bash
+++ install-superlu.bash
@@ -20,31 +20,24 @@ if ! [ -d "superlu" ]; then
 fi
 cd superlu
 
-# function to compile and copy results to local directories
-action () {
-    local dir=$1
-    cd $dir
-    make local
-    make install
-    cd ..
-}
+# run cmake
+cmake . \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D BUILD_SHARED_LIBS=ON \
+    -D TPL_BLAS_LIBRARIES=openblas \
+    -D TPL_ENABLE_METISLIB=ON \
+    -D TPL_METIS_INCLUDE_DIRS="/usr/include" \
+    -D TPL_METIS_LIBRARIES="metis"
 
-# compile and copy results to local directories
-action SuiteSparse_config
-action AMD
-action CAMD
-action CCOLAMD
-action COLAMD
-action CHOLMOD
-action SuperLU
+make && make test
 
 # copy include files
 sudo mkdir -p $INCDIR/
-sudo cp -av include/*.h $INCDIR/
+sudo cp -av SRC/*.h $INCDIR/
 
 # copy libray files
 sudo mkdir -p $LIBDIR/
-sudo cp -av lib/* /usr/local/lib/superlu
+sudo cp -av SRC/*.so* /usr/local/lib/superlu/
 
 # update ldconfig
 echo "${LIBDIR}" | sudo tee /etc/ld.so.conf.d/superlu.conf >/dev/null
